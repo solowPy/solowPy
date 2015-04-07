7197c8e58908bfa93c3545ac13e6e453f251c69e
David R. Pugh <drobert.pugh@gmail.com>
2015-04-07 16:40:22 +0100

    Added a version writer function to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,30 @@
 from setuptools import setup
+import os
 
-import solowpy
+#-Write Versions File-#
+#~~~~~~~~~~~~~~~~~~~~~#
+
+VERSION = '0.1.0-alpha'
+
+def write_version_py(filename=None):
+    """ This constructs a version file for the project """
+    doc = "\"\"\"This is a VERSION file and should NOT be manually altered\"\"\""
+    doc += "\nversion = '%s'" % VERSION
+    
+    if not filename:
+        filename = os.path.join(os.path.dirname(__file__), 'quantecon', 'version.py')
+    
+    fl = open(filename, 'w')
+    try:
+        fl.write(doc)
+    finally:
+        fl.close()
+
+write_version_py()  # This is a file used to control the solowpy.__version__ attribute
+
+
+#-Setup-#
+#~~~~~~~#
 
 setup(
     name="solowPy",
