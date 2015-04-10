f114409052383c0d935c10906474f681e6287fb6
David R. Pugh <drobert.pugh@gmail.com>
2015-04-10 05:54:53 +0100

    Minor revisions to setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,51 @@
 from setuptools import setup
 import os
 
-#-Write Versions File-#
-#~~~~~~~~~~~~~~~~~~~~~#
-
+# Write versions file
 VERSION = '0.1.0-alpha'
+
 
 def write_version_py(filename=None):
     """ This constructs a version file for the project """
     doc = "\"\"\"This is a VERSION file and should NOT be manually altered\"\"\""
     doc += "\nversion = '%s'" % VERSION
-    
+
     if not filename:
         filename = os.path.join(os.path.dirname(__file__), 'solowpy', 'version.py')
-    
+
     fl = open(filename, 'w')
     try:
         fl.write(doc)
     finally:
         fl.close()
 
-write_version_py()  # This is a file used to control the solowpy.__version__ attribute
+write_version_py()  # used to control the solowpy.__version__ attribute
 
+# Meta information
+DESCRIPTION = ("Library for solving, simulating, and estimating the " +
+               "Solow (1956) model of economic growth.")
 
-#-Setup-#
-#~~~~~~~#
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Education',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.3',
+               'Programming Language :: Python :: 3.4',
+               'Topic :: Scientific/Engineering',
+               ]
 
+# Setup config
 setup(
     name="solowPy",
     version=VERSION,
     license="MIT License",
     author="davidrpugh",
+    description=DESCRIPTION,
     install_requires=["numpy",
                       "scipy",
                       "sympy",
@@ -38,11 +53,5 @@ setup(
                       "quantecon",
                       ],
     author_email="david.pugh@maths.ox.ac.uk",
-    classifiers=['Development Status :: 1 - Planning',
-                 'Intended Audience :: Education',
-                 'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: MIT License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 ]
+    classifiers=CLASSIFIERS
     )
