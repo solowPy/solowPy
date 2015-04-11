32b353aa68ba54d421100fd835ae53a72c2e8cc4
David R. Pugh <drobert.pugh@gmail.com>
2015-04-11 07:48:51 +0100

    Added a citation file and made some edits to the setup.py

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ solowPy
 =======
 [![Build Status](https://travis-ci.org/solowPy/solowPy.svg?branch=master)](https://travis-ci.org/solowPy/solowPy)
 [![Coverage Status](https://coveralls.io/repos/solowPy/solowPy/badge.svg)](https://coveralls.io/r/solowPy/solowPy)
+[![Development Status](https://pypip.in/status/solowPy/badge.svg)](https://pypi.python.org/pypi/solowPy/)
 [![DOI](https://zenodo.org/badge/doi/10.5281/zenodo.16720.svg)](http://dx.doi.org/10.5281/zenodo.16720)
 
 Library for solving, simulating, and estimating the [Solow (1956)](http://piketty.pse.ens.fr/files/Solow1956.pdf) model of economic growth.
--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
+import os
+
 from distutils.core import setup
 
-# Meta information
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+
 DESCRIPTION = ("Library for solving, simulating, and estimating the " +
                "Solow (1956) model of economic growth.")
 
@@ -15,19 +23,19 @@ CLASSIFIERS = ['Development Status :: 3 - Alpha',
                'Programming Language :: Python :: 2.7',
                'Programming Language :: Python :: 3.3',
                'Programming Language :: Python :: 3.4',
-               'Topic :: Scientific/Engineering',
                ]
 
-# Setup config
 setup(
-    name="solowpy",
-    packages=['solowpy',
-              'solowpy.tests'],
-    version='0.1.3-alpha',
-    license="MIT License",
-    author="davidrpugh",
-    description=DESCRIPTION,
-    author_email="david.pugh@maths.ox.ac.uk",
-    url='https://github.com/solowPy/solowPy',  # url to the repo
-    classifiers=CLASSIFIERS
-    )
+      name="solowpy",
+      packages=['solowpy',
+                'solowpy.tests'],
+      version='0.1.3-alpha',
+      license="MIT License",
+      author="davidrpugh",
+      author_email="david.pugh@maths.ox.ac.uk",
+      url='https://github.com/solowPy/solowPy',
+      description=DESCRIPTION,
+      long_description=(read('README.md') + '\n\n' +
+                        read('CITATION')),
+      classifiers=CLASSIFIERS,
+      )
