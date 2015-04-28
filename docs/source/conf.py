a1fb7b28744c285135676d57182fa24253e42c79
David R. Pugh <drobert.pugh@gmail.com>
2015-04-28 19:19:19 +0100

    Try mocks but now requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,18 +15,18 @@
 import sys
 import os
 
-# from mock import Mock as MagicMock
+from mock import Mock as MagicMock
 
 
-# class Mock(MagicMock):
-#     @classmethod
-#     def __getattr__(cls, name):
-#             return Mock()
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
 
-# MOCK_MODULES = ['scipy', 'scipy.optimize', 'sympy', 'numpy', 'pandas',
-#                 'quantecon', 'quantecon.ivp', 'matplotlib', 'matplotlib.pyplot',
-#                 'numba']
-# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+MOCK_MODULES = ['scipy', 'scipy.optimize', 'sympy', 'numpy', 'pandas',
+                'quantecon', 'quantecon.ivp', 'matplotlib', 'matplotlib.pyplot',
+                'numba']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
