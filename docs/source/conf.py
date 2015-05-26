0359833adddabaaedd9771d82cb8f2b1ad9536e4
David R. Pugh <drobert.pugh@gmail.com>
2015-05-26 06:00:06 +0100

    Try again to build RTD.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['scipy', 'scipy.optimize', 'sympy', 'numpy', 'pandas',
-                'quantecon', 'quantecon.ivp', 'matplotlib', 'matplotlib.pyplot',
-                'numba']
+MOCK_MODULES = ['quantecon', ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
